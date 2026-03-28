ab4387ff582990e53e9c62ce0fa67fb0be5e6a1c
garidana-collab <garidana@gmail.com>
2026-03-28 19:48:05 +0900

    상욱
    GameOver UI 작업 중

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,32 @@
+using TMPro;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 점수
+    public TextMeshProUGUI scoreText;
+    // 게임 오버 UI
+    public GameObject gameOverUi;
+
+    private int score = 0;
+    public bool IsGameOver {get; private set;}
+
+    public void Awake()
     {
-        
+        gameOverUi.SetActive(false);
     }
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        // // 게임오버 시 화면 씬 리셋
+        // if (IsGameOver && Input.GetKeyDown(KeyCode.Space));
+        // {
+        //     // 이게 아직 잘 모르겠음;;
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // }
     }
 }
